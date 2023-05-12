fd765fc72973bdcefd24a89572dedb1ebb612d0f
Yamayamaaya <yugo139013@gmail.com>
2023-05-12 15:39:38 +0900

    Login with google

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import CreatePost from './components/CreatePost';
 import Login from './components/Login';
 import Logout from './components/Logout';
 import Navbar from './components/Navbar';
+import { useState } from 'react';
 
 
 function App() {
+  //ログインしているかどうかをusestateで管理
+  const [isAuth, setIsAuth] = useState(false);
+
   return (
       <Router>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create-post" element={<CreatePost />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
           <Route path="/logout" element={<Logout />} />
         </Routes>
       </Router>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,32 @@
+import { signInWithPopup } from 'firebase/auth';
 import React from 'react';
+import { auth, provider } from '../firebase';
+import { useNavigate } from 'react-router-dom';
 
-const Login = () => {
+const Login = ({setIsAuth}) => {
+    const navigate = useNavigate();
+    const loginWithGoogle = () => {
+        //googleでログイン
+        signInWithPopup(auth, provider).then((result) => {
+            // ログイン成功時の処理
+            // ローカルストレージに保存
+            localStorage.setItem('isAuth', true);
+            setIsAuth(true);
+            console.log(result);
+            // ログイン後に遷移する
+            navigate('/');
+        }
+        ).catch((error) => {
+            // ログイン失敗時の処理
+            console.log(error);
+        }
+        );
+    }
     return (
         <div>
             <h1>Login</h1>
-            <p>This is the login page.</p>
+            <p>Login to start</p>
+            <button onClick = {loginWithGoogle}>Googleでログイン</button>
         </div>
     );
 }
