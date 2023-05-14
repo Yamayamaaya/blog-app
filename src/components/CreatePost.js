1e1a21c08e0734ee122a3b5dc9d66adca736d555
Yamayamaaya <yugo139013@gmail.com>
2023-05-14 19:51:50 +0900

    create createPost page

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import './CreatePost.css';
+import { useState } from 'react';
 
 const CreatePost = () => {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    const createPost = () => {
+        const post = {title, content};
+        console.log(post);
+    }   
+
+    
     return (
         <div>
             <h1>Create Post</h1>
             <p>This is the create post page.</p>
+            
+            <div className="postContainer">
+                <h3>記事を投稿する</h3>
+                <div className="inputPost">
+                    <input type="text" placeholder="タイトル" onChange={(e) => setTitle(e.target.value)}/>
+                    <textarea placeholder="投稿内容を記入" onChange={(e) => setContent(e.target.value)}/>
+                    <button className="postButton" onClick={createPost}>投稿</button>
+                </div>
+            </div>
         </div>
     );
 }
